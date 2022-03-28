6a02c9bb482bba45f1a71afe6e4c88b707ddf97d
micaetranquocbao <micaetranquocbao2001@gmail.com>
2022-03-28 13:19:24 +0700

    Done userroutes

--- readme.md
+++ readme.md
@@ -1,7 +1,21 @@
-# Step by step to setup api
+# Setup backend
+```javascript
 npm init
 npm i bcryptjs dotenv express express-async-handler jsonwebtoken mongoose morgan
 npm i --dev concurrently nodemon
--Code: server express
--Code: config mongodb
--Code:
+```
+# Note
+| Module            | PORT          | Port  |
+| ----------------- |:-------------:| -----:|
+| BE (API)          | /api/v1/*     | 3000  |
+| FE web (client)   | /*            | 5000  |
+| FE web (admin)    | /admin/*      | 5000  |
+
+# .env file
+```
+PORT=<port_here>
+NODE_ENV=<development||production>
+JWT_SECRET=<jwt_secret_key>
+PAYPAL_CLIENT_ID=<bảo cute vl>
+MONGO_URL=<mongo_uri_here>
+```
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -33,17 +33,96 @@ userRouter.post(
 /**
  * REGISTER
  */
+userRouter.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const isExistingUser = User.find({ email });
+    if (isExistingUser) {
+      res.status(400);
+      throw new Error("Email of user already exists");
+    }
+    //else
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+    if (newUser) {
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: generateToken(newUser._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  })
+);
 
 /**
  * PROFILE
  */
+userRouter.get(
+  "/profile",
+  protect,
+  expressAsyncHandler(async (res, req) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        createAt: user.createAt,
+      });
+    } else {
+      res.status(400);
+      throw new Error("User not Found");
+    }
+  })
+);
 
 /**
  * UPDATE PROFILE
  */
+userRouter.put("/profile", protect, async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.express = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      createAt: updateUser.createAt,
+      token: generateToken(updateUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not Found");
+  }
+});
 
 /**
  * GET ALL USER by ADMIN
  */
+userRouter.get(
+  "/",
+  protect,
+  admin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+  })
+);
 
 export default userRouter;
