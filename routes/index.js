d0c4299bd1e89175471aac0a2f1b3cbde824a924
micaetranquocbao <micaetranquocbao2001@gmail.com>
2022-06-23 11:07:14 +0700

    update infor

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,16 @@ import cartRouter from "./cartRoutes.js";
 import commentRouter from "./commentRoutes.js";
 
 const routes = (app) => {
-  app.use("/api/v1/import", ImportData);
-  app.use("/api/v1/order", orderRouter);
-  app.use("/api/v1/product", productRouter);
-  app.use("/api/v1/user", userRouter);
-  app.use("/api/v1/category", categoryRouter);
-  app.use("/api/v1/producer", producerRouter);
-  app.use("/api/v1/cart", cartRouter);
-  app.use("/api/v1/comment", commentRouter);
-  app.use("/api/v1/config/paypal", (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID);
-  });
+    app.use("/api/v1/import", ImportData);
+    app.use("/api/v1/order", orderRouter);
+    app.use("/api/v1/product", productRouter);
+    app.use("/api/v1/user", userRouter);
+    app.use("/api/v1/category", categoryRouter);
+    app.use("/api/v1/producer", producerRouter);
+    app.use("/api/v1/cart", cartRouter);
+    app.use("/api/v1/comment", commentRouter);
+    app.use("/api/v1/config/paypal", (req, res) => {
+        res.send(process.env.PAYPAL_CLIENT_ID);
+    });
 };
 export default routes;
